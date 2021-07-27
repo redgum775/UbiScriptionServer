463117c9f72313d317403841478834902f47b040
redgum <redgum775@gmail.com>
2021-07-27 12:34:30 +0900

    Add the ability to send instructions to speakers.

--- src/speaker/Speaker.java
+++ src/speaker/Speaker.java
@@ -1,5 +1,11 @@
 package src.speaker;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 import com.google.gson.Gson;
 
 import src.speaker.state.State;
@@ -8,15 +14,56 @@ public class Speaker {
   String  address = null;  // スピーカーのIPアドレス
   Integer port = null;     // スピーカーのポート番号
 
+	Socket socket = null;
+	BufferedReader reader = null;
+	PrintWriter writer = null;
+
   State state = null;      // スピーカーの状態
   
   public Speaker(){
-    address = "192.168.1.1";
-    port = 5000;
+    address = "192.168.1.7";
+    port = 5010;
+  }
+
+  public void connection(){
+    try {
+      socket = new Socket(address, port);
+      writer = new PrintWriter(socket.getOutputStream(), true);
+      reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
+  public void disconnection(){
+      try {
+        if(socket != null) socket.close();
+        if(writer != null) writer.close();
+        if(reader != null) reader.close();
+      } catch (IOException e) {
+        e.printStackTrace();
+      }
   }
 
   public void sendData(){
-    Gson gson = new Gson();
-    String json = gson.toJson(state);
+    if(state != null){
+      Gson gson = new Gson();
+      String json = gson.toJson(state);
+      try{
+        writer.println("[" + json + "]");
+        System.out.println("Sended: [" + json + "]");
+        System.out.println("Received: " + reader.readLine());
+        writer.println("q^");
+        System.out.println("sended: q^");
+        System.out.println("Received: " + reader.readLine());
+      }catch(Exception e){
+        e.printStackTrace();
+      }
+    }
+  }
+
+  public void setPlayText(String text){
+    state = new State();
+    state.setText(text);
   }
 }
--- src/speaker/state/State.java
+++ src/speaker/state/State.java
@@ -1,30 +1,28 @@
 package src.speaker.state;
 
 public class State {
-  private Boolean play = null;
-  private Boolean pause = null;
-  private Boolean stop = null;
   private Integer volume = null;
+  private Integer rate = null;
   private String text = null;
-
-  public Boolean getPlay() {
-    return play;
-  }
-
-  public void setPlay(Boolean play) {
-    this.play = play;
-  }
   
   public Integer getVolume() {
-    return volume;
+    return this.volume;
   }
 
   public void setVolume(Integer volume) {
     this.volume = volume;
   }
 
+  public Integer getRate() {
+    return this.rate;
+  }
+
+  public void setRate(Integer rate) {
+    this.rate = rate;
+  }
+
   public String getText() {
-    return text;
+    return this.text;
   }
 
   public void setText(String text) {
